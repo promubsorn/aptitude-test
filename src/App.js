a3a4735b05c72cd0f72ea47504d52db035b9bc3d
promubsorn <nammon775@gmail.com>
2025-07-21 17:19:03 +0700

    add react-router-dom for routing and update App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
 import MentalMathApp from './components/mental-math-trainer';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import ErrorBoundary from './components/error-boundary';
-const theme = createTheme(); // You can customize the theme here if needed
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <ErrorBoundary>
-        <div className="App">
-          <MentalMathApp />
-        </div>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <BrowserRouter basename="/mental-math">
+      <MentalMathApp />
+    </BrowserRouter>
   );
 }
 
